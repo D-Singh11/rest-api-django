520593235c52448ee2f989265e2617cdd2fa1b8d
D-Singh11 <sssscorejava@gmail.com>
2024-05-02 00:41:29 +0100

    Add new url pattern to handle bog by id

--- mysite/api/urls.py
+++ mysite/api/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('blogs', views.BlogPostListCreate.as_view(), name='blogpost-view-craete')
+    path('blogs', views.BlogPostListCreate.as_view(), name='blogpost-view-craete'),
+    path('blogs/<int:pk>', views.BlogPostRetrieveUpdateDestroy.as_view(), name='blogpost-view-retrieve-update-destroy')
 ]
